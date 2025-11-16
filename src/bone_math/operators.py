743442cdbf1ecb002f2b4cb091f440195fb3dd82
Xury Greer <Xury.Greer@Gmail.com>
2025-11-16 06:10:43 -0800

    Implement operator for calculating and setting the correct pole angle for IK chains

--- src/bone_math/operators.py
+++ src/bone_math/operators.py
@@ -1,40 +1,120 @@
 __author__ = "Xury Greer"
 
 # Built-ins
-from typing import TYPE_CHECKING
+from typing import (
+    TYPE_CHECKING,
+    Literal,
+    cast,
+)
 
 # Third Party
 import bpy
 from bpy.types import (
+    Armature,
     Context,
+    KinematicConstraint,
+    Object,
     Operator,
+    PoseBone,
 )
 
 # First Party
 from .pole_angle import (
-    placeholder,
+    get_pole_angle,
 )
 
 if TYPE_CHECKING:
     # https://b3d.interplanety.org/en/how-to-properly-specify-the-return-type-of-the-blender-operator-execute-function/
-    from bpy.stub_internal.rna_enums import OperatorReturnItems
+    from bpy.stub_internal.rna_enums import (
+        OperatorReturnItems,
+    )
+
+    PosePositionItems = Literal["POSE", "REST"]
 
 
 class OT_BoneMath_CalculatePoleAngle(Operator):
-    """Calculate and set the correct pole angle for the selected pose bones with Inverse Kinematics constraints."""
+    """Calculate and set the correct pole angle for all selected pose bones that have Inverse Kinematics constraints."""
 
     bl_idname = "bone_math.calculate_pole_angle"
-    bl_label = "Calculate pole angle"
+    bl_label = "Calculate Pole Angle"
+    bl_icon = "CON_ROTLIMIT"
+
+    @classmethod
+    def poll(cls, context: Context) -> bool:
+        if context.mode != "POSE":
+            return False
+
+        pose_bones: list[PoseBone] = context.selected_pose_bones
+        if not pose_bones:
+            return False
+
+        armature_object: Object | None = context.active_object
+        if armature_object is None:
+            return False
+
+        return True
 
     def execute(self, context: Context) -> set["OperatorReturnItems"]:
-        placeholder(context)
+        pose_bones: list[PoseBone] = context.selected_pose_bones
+        armature_object: Object = cast(Object, context.active_object)
+        armature: Armature = cast(Armature, armature_object.data)
+
+        # Cache the pose position the user had the armature set to.
+        original_pose_position: PosePositionItems = armature.pose_position
+
+        for pose_bone in pose_bones:
+            # set_pole_angle(context, armature_object, pose_bone)
+
+            ik_constraint: KinematicConstraint | None = None
+            for constraint in pose_bone.constraints:
+                if constraint.type == "IK":
+                    ik_constraint = cast(KinematicConstraint, constraint)
+                    break
+            else:
+                continue
+
+            base_bone: PoseBone = pose_bone
+
+            chain_length: int = ik_constraint.chain_count
+            if chain_length == 0:
+                # With a chain length of 0,
+                # the chain keeps going until it reaches a bone with no parent.
+                while base_bone.parent:
+                    base_bone = base_bone.parent
+            else:
+                # Walk up the bone parent hierarchy to find the start of the chain.
+                for _ in range(1, ik_constraint.chain_count):
+                    base_bone = base_bone.parent
+
+            if base_bone is None:
+                return {"CANCELLED"}
+
+            pole_bone = ik_constraint.pole_target
+            if pole_bone is None:
+                return {"CANCELLED"}
+
+            if pole_bone.type == "ARMATURE":
+                pole_bone = armature_object.pose.bones[ik_constraint.pole_subtarget]
+
+            if pole_bone is None:
+                return {"CANCELLED"}
+
+            if (base_bone is not None) and (pole_bone is not None):
+                # Switch to rest pose and force an update so we can work with the bones in their resting positions.
+                armature.pose_position = "REST"
+                bpy.context.view_layer.update()
+
+                pole_angle: float = get_pole_angle(base_bone, pose_bone, pole_bone.matrix.translation)
+
+                # Switch back to the pose position the user had previously.
+                armature.pose_position = original_pose_position
+
+                ik_constraint.pole_angle = pole_angle
 
         return {"FINISHED"}
 
 
-classes: list[type] = [
-    OT_BoneMath_CalculatePoleAngle,
-]
+classes: list[type] = [OT_BoneMath_CalculatePoleAngle]
 
 
 def register() -> None:
--- src/bone_math/pole_angle.py
+++ src/bone_math/pole_angle.py
@@ -1,16 +1,71 @@
+"""
+Adapted from: https://blender.stackexchange.com/a/19755
+by Jaroslav Jerryno Novotny
+"""
+
 __author__ = "Xury Greer"
 
 # Third Party
-import bpy
 from bpy.types import (
-    Context,
+    PoseBone,
+)
+from mathutils import (
+    Vector,
 )
 
 
-def placeholder(context: Context) -> None:
-    """Placeholder function.
+def signed_angle(
+    vector_a: Vector,
+    vector_b: Vector,
+    normal: Vector,
+) -> float:
+    """Get the signed angle between two vectors.
 
-    :param context: The current Blender context.
+    :param vector_a: The first vector.
+    :param vector_b: The second vector.
+    :param normal: Sign will be determined by this orientation.
+    :return: The signed angle between the two vectors around the given normal axis.
     """
 
-    print(context.active_object)
+    angle: float = vector_a.angle(vector_b)
+    if vector_a.cross(vector_b).angle(normal) < 1.0:
+        angle = -angle
+    return angle
+
+
+def get_pole_angle(
+    start_bone: PoseBone,
+    end_bone: PoseBone,
+    pole_location: Vector,
+) -> float:
+    """Calculate the pole angle for bones in an IK chain.
+    The resulting angle will align the IK chain so that the bones don't move from their rest positions.
+
+    Pole angle is a signed angle between the start_bone's X axis and
+    the projected pole_axis onto start_bone's XZ plane.
+    The direction of projection is IK_axis.
+
+    :param start_bone: The first bone in the IK chain.
+    :param end_bone: The final bone in the IK chain.
+    :param pole_location: The location of the pole target.
+    :return: The signed angle value in radians that is required to make the IK chain aim
+        at the pole target so that it matches the resting pose.
+    """
+
+    # Axis aimed from the base of the chain to the end of the chain.
+    start_to_end: Vector = end_bone.tail - start_bone.head
+
+    # Axis aimed from the base of the chain, to the pole target.
+    start_to_pole: Vector = pole_location - start_bone.head
+
+    pole_normal: Vector = start_to_end.cross(start_to_pole)
+
+    projected_pole_axis: Vector = pole_normal.cross(start_bone.y_axis)
+
+    pole_angle: float = signed_angle(
+        start_bone.x_axis,
+        projected_pole_axis,
+        start_bone.y_axis,
+    )
+
+    return pole_angle
